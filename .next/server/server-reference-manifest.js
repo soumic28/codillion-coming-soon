149dfcaae599ce552404a5f048200e8b5fe68fa9
soumic sarkar <soumicdev@gmail.com>
2025-02-27 15:31:49 +0530

    commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HNb2YvybxqdJiFWacpRNBsByVYZlV1boofArWObGfMo=\"\n}"
---
+++ .next/static/chunks/src_app_page_tsx_8b298505._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_8b298505._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_8b298505._.js",
+  "chunks": [
+    "static/chunks/node_modules_fbc3701f._.js",
+    "static/chunks/src_ced69a32._.js"
+  ],
+  "source": "dynamic"
+});
